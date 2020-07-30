f2f365c0177e063968ff56ddb03c4b614d968e1d
ari7946 <mercado7946@gmail.com>
2020-07-29 17:11:35 -0700

    Normalize data

--- tuhoradivina/src/components/data/data.js
+++ tuhoradivina/src/components/data/data.js
@@ -6,27 +6,46 @@ import sixteenFourth from '../../assets/audio/07-Enero/23-01-2016.mp3'
 
 const data = {
   sixteen: {
-    id: 1,
     yearTitle: '2016: Elige Mes',
-    monthTitle: 'Enero 2016',
-    months: ['enero', 'febrero', 'marzo', 'april', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
-    audioData: [{
-      audioTitle: "02-01-2016",
-      // audioPath: "../../../../audio/07-Enero/02-01-2016.mp3",
-      audioFile: sixteenFirst
-    },
-    {
-      audioTitle: "09-01-2016",
-      audioFile: sixteenSecond
-    },
-    {
-      audioTitle: "16-01-2016",
-      audioFile: sixteenThird,
-    },
-    {
-      audioTitle: "23-01-2016",
-      audioFile: sixteenFourth
-    }],
+    allMonths: ['enero', 'febrero', 'marzo', 'april', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+    data: {
+      enero: {
+        id: 1,
+        monthTitle: 'Enero 2016',
+        audioData: [{
+          audioTitle: "02-01-2016",
+          audioFile: sixteenFirst
+        }, {
+          audioTitle: "09-01-2016",
+          audioFile: sixteenSecond
+        }, {
+          audioTitle: "16-01-2016",
+          audioFile: sixteenThird,
+        }, {
+          audioTitle: "23-01-2016",
+          audioFile: sixteenFourth
+        }],
+      },
+
+      febrero: {
+        id: 2,
+        monthTitle: 'Febrero 2016',
+        audioData: [{
+          audioTitle: "02-01-2016",
+          audioFile: sixteenFirst
+        }, {
+          audioTitle: "09-01-2016",
+          audioFile: sixteenSecond
+        }, {
+          audioTitle: "16-01-2016",
+          audioFile: sixteenThird,
+        }, {
+          audioTitle: "23-01-2016",
+          audioFile: sixteenFourth
+        }],
+      },
+
+    }    
   }
 }
 
